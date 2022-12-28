1c6b102c88002b4e5fbaf3d3c3894d638ac0b05e
Abdulaziz <mohammed.abdulazizt.19@gmail.com>
2022-12-28 14:57:04 +0800

    get the dev environoment from github

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item"><router-link class="nav-link active" :to="{name: 'home'}">Home</router-link></li>
+        <li class="nav-item"><router-link class="nav-link" :to="{name: 'about'}">About</router-link></li>
+      </ul>
+    </div>
+  </nav>
+<router-view />
+</template>
+
+<script setup>
+import {RouterView, RouterLink} from 'vue-router'
+
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam explicabo sunt minus dolorem quo. Delectus, nobis? Incidunt, provident quia vel architecto rem est natus illo, facilis autem laudantium explicabo nulla?</p>
+    <button type="button" class="btn btn-primary">Primary</button>
+</template>
